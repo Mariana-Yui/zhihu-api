5456f728a33236134db3c5f2d2cf3056fd8fc97c
Mariana <HealMSlin@gmail.com>
2019-09-26 20:54:29 +0800

    feat: 更新

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -5,13 +5,13 @@ class HomeController {
         ctx.body = `
             <form action="/upload" enctype="multipart/form-data" method="POST">
                 <label for="file">上传图片</label>
-                <input type="file" id="file" />
-                <input type="submit" value="提交" />
+                <input type="file" id="file" name="file" accept=".png, .jpg, .jpeg" />
+                <button type="submit">提交</button>
             </form>
         `;
     }
     public async upload(ctx: any) {
-        const file = ctx.request.files && ctx.request.files[""];
+        const file = ctx.request.files && ctx.request.files.file;
         const basename = path.basename(file.path);
         ctx.body = { url: `${ctx.origin}/upload/${basename}` };
     }
